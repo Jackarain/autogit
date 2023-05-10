c9d0076b5701f427bc33a2dbb30bd5a246762499
jackarain <jack.wgm@gmail.com>
2023-05-11 00:36:35 +0800

    Add some files

--- autogit/src/main.cpp
+++ autogit/src/main.cpp
@@ -11,23 +11,57 @@
 
 #include "autogit/logging.hpp"
 
+#include <cstdio>
+#include <cstdlib>
+
+#include <string>
+#include <tuple>
+
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
+
+#ifdef _MSC_VER
+# define popen _popen
+# define pclose _pclose
+#endif // _MSC_VER
+
+std::tuple<std::string, bool> run_command(const std::string& cmd) noexcept
+{
+	auto pf = popen(cmd.c_str(), "r");
+	if (!pf)
+		return { "", false };
+
+	std::string result;
+	size_t total = 0;
+
+	while (!feof(pf))
+	{
+		result.resize(total + 1024);
+		auto nread = fread((char*)(result.data() + total), 1, 1024, pf);
+		if (nread <= 0)
+			break;
+		total += nread;
+	}
+	result.resize(total);
+
+	int exit_code = pclose(pf);
+	return { result, exit_code == EXIT_SUCCESS ? true : false };
+}
 
 
 int main(int argc, char** argv)
 {
-	std::string bot_token;
-	std::string bot_server;
+	std::string git_repository;
 	std::string log_directory;
+	int time;
 	bool disable_logs = false;
 
 	// 解析命令行.
 	po::options_description desc("Options");
 	desc.add_options()
 		("help,h", "Help message.")
-		("bot-token", po::value<std::string>(&bot_token)->value_name("token"), "Telegram bot token id")
-		("bot-server", po::value<std::string>(&bot_server)->default_value("https://api.telegram.org")->value_name("url"), "Telegram bot server url")
+		("repository", po::value<std::string>(&git_repository)->value_name("repository"), "Git repository path")
+		("time", po::value<int>(&time)->default_value(false), "Wait time seconds until committing")
 		("disable_logs", po::value<bool>(&disable_logs)->default_value(false), "Disable logs")
 		("logs_path", po::value<std::string>(&log_directory)->value_name(""), "Logs dirctory.")
 		;
@@ -58,6 +92,8 @@ int main(int argc, char** argv)
 	util::init_logging(log_directory);
 
 	LOG_DBG << "Running...";
+
+
 
 	return EXIT_SUCCESS;
 }
