acbf467baa6d6bd752f96576a12356c729f869d9
jackarain <jack.wgm@gmail.com>
2023-05-14 01:36:22 +0800

    Fix missing set_thread_name

--- autogit/src/main.cpp
+++ autogit/src/main.cpp
@@ -52,6 +52,7 @@ namespace po = boost::program_options;
 # define sleep(a) Sleep(a * 1000)
 #else
 # include <unistd.h>
+# include <sys/prctl.h>
 # include <sys/resource.h>
 #endif
 
@@ -508,6 +509,57 @@ boost::asio::awaitable<int> co_main(int argc, char** argv)
 	co_return EXIT_SUCCESS;
 }
 
+#ifdef WIN32
+
+const DWORD MS_VC_EXCEPTION = 0x406D1388;
+
+#pragma pack(push,8)
+typedef struct tagTHREADNAME_INFO
+{
+	DWORD dwType; // Must be 0x1000.
+	LPCSTR szName; // Pointer to name (in user addr space).
+	DWORD dwThreadID; // Thread ID (-1=caller thread).
+	DWORD dwFlags; // Reserved for future use, must be zero.
+} THREADNAME_INFO;
+#pragma pack(pop)
+
+void SetThreadName(uint32_t dwThreadID, const char* threadName)
+{
+#if __MINGW32__
+	(void)dwThreadID;
+	(void)threadName;
+#else
+	THREADNAME_INFO info;
+	info.dwType = 0x1000;
+	info.szName = threadName;
+	info.dwThreadID = dwThreadID;
+	info.dwFlags = 0;
+
+	__try
+	{
+		RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+	}
+#endif
+}
+
+void set_thread_name(const char* name)
+{
+	SetThreadName(GetCurrentThreadId(), name);
+}
+
+#elif __linux__
+
+void set_thread_name(const char* name)
+{
+	prctl(PR_SET_NAME, name, 0, 0, 0);
+}
+
+
+#endif
+
 static int platform_init()
 {
 #if defined(WIN32) || defined(_WIN32)
@@ -572,7 +624,7 @@ static int platform_init()
 
 #ifdef __linux__
 	signal(SIGPIPE, SIG_IGN);
-#else
+#elif WIN32
 	set_thread_name("mainthread");
 #endif
 	return 0;
